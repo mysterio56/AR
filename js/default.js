54a9e6f029d8e6b05462780710b76ea09fc3303e
Francisco Javier Sánchez Rosales <aslanlion56@gmail.com>
2013-04-17 11:10:43 -0500

    Update default.js

--- js/default.js
+++ js/default.js
@@ -7,5 +7,6 @@ function functionDefault(){
 function otherFunction(){
     var sMenssage = "other function";
     console.log(sMenssage+';'); // esto es un comentario en github for windows
-    //conflicto github 2
+    //conflicto github 3
+    //otro comentario
 }
